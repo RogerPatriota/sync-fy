ed00b5ee703d85ee6375321897544707bfd73fb1
roger <rogerpatriota@gmail.com>
2025-11-30 20:32:27 -0300

    feat(front-end): add home page hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { ArrowDown } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello Sync-fy</div>
+    <div className="min-h-screen bg-[url(/v882-kul-35.jpg)] bg-cover bg-center">
+      <div className="flex flex-col min-h-screen items-center justify-center">
+        <h1 className="text-zinc-400 text-8xl font-hero">Transform a
+          <span className="text-green-600"> Spotify </span><br/> 
+          playlist to a 
+          <span className="text-red-600"> YouTube</span>
+        </h1>
+
+        <div className="bg-green-600 rounded-full p-2">
+          <ArrowDown strokeWidth={3}/>
+        </div>
+                
+      </div>
+    </div>
   )
 }
